f25dd50a077f67892308a00b1c3c73644beb4d00
John Dale <unitejohndalev@gmail.com>
2023-05-30 14:57:44 +0800

    CRUD using PHP and XAMPP

---
+++ index.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>CRUD</title>
+  </head>
+  <body>
+   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="index.php">PHP CRUD OPERATION</a>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+            </li>
+            <li class="nav-item">
+              <a type="button" class="btn btn-primary nav-link active" href="create.php">Add New</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+   <div class="container my-4">
+    <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>NAME</th>
+        <th>EMAIL</th>
+        <th>PHONE</th>
+        <th>JOINING DATE</th>
+        <th>ACTIONS</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        include "connection.php";
+        $sql = "select * from crud1";
+        $result = $conn->query($sql);
+        if(!$result){
+          die("Invalid query!");
+        }
+        while($row=$result->fetch_assoc()){
+          echo "
+      <tr>
+        <th>$row[id]</th>
+        <td>$row[name]</td>
+        <td>$row[email]</td>
+        <td>$row[phone]</td>
+        <td>$row[join_date]</td>
+        <td>
+                  <a class='btn btn-success' href='edit.php?id=$row[id]'>Edit</a>
+                  <a class='btn btn-danger' href='delete.php?id=$row[id]'>Delete</a>
+                </td>
+      </tr>
+      ";
+        }
+      ?>
+    </tbody>
+  </table>
+      </div>
+ 
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  </body>
+</html>
